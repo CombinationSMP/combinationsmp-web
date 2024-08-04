fc23ccc5ef8fcf228c1917ddcca78a567ab2bb5d
Everly Scott <everly@everly.page>
2024-08-03 23:55:39 -0500

    Revert "auto deploy testing"
    This reverts commit b129ff1536766007aecd4fcff764691ab5d70fa6.

--- src/app/health/route.ts
+++ src/app/health/route.ts
@@ -4,7 +4,3 @@ import { NextResponse } from "next/server";
 export const GET: Handler = () => {
   return new NextResponse("healthy");
 };
-
-export const POST: Handler = () => {
-  return new NextResponse("This is an example to test automatic deployments");
-};
