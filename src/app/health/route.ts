b129ff1536766007aecd4fcff764691ab5d70fa6
Everly Scott <everly@everly.page>
2024-08-03 23:42:36 -0500

    auto deploy testing

--- src/app/health/route.ts
+++ src/app/health/route.ts
@@ -4,3 +4,7 @@ import { NextResponse } from "next/server";
 export const GET: Handler = () => {
   return new NextResponse("healthy");
 };
+
+export const POST: Handler = () => {
+  return new NextResponse("This is an example to test automatic deployments");
+};
